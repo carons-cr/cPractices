3d089b0260608fd4965a88cabcd2352b83dee743
carons-cr <2738794789@qq.com>
2019-02-22 15:01:36 +0800

    complete example1, calculate the sum of the area of two rectangles.

---
+++ chapter1_examples/example2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+double area(double h, double w)
+{
+  double s;
+  s = h * w;
+  return s;  
+}
+
+int main()
+{
+  double h1, h2, w1, w2, s1, s2;
+  h1 = 10.5;
+  w1 = 20.5;
+  h2 = 1.5 * h1;
+  w2 = 1.5 * w1;
+  s1 = area(h1, w1);
+  s2 = area(h2, w2);
+  printf("area = %6.2f\n", s1 + s2); 
+  return 0;
+}
