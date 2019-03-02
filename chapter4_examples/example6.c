c7552b934ed527a841bdf6b1dbba527412049c8e
carons-cr <2738794789@qq.com>
2019-03-02 10:45:09 +0800

    complete example6, integer and normal character scanf.

---
+++ chapter4_examples/example6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 0, b = 0, c = 0;
+  printf("Input a = , b = , c = \n");
+  scanf("a = %d, b = %d, c = %d", &a, &b, &c);
+  printf("a = %d, b = %d, c = %d\n", a, b, c);
+  return 0;
+}
