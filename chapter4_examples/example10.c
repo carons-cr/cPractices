e376cba0b383a7d2f56586a76d0d46027f9cce2d
carons-cr <2738794789@qq.com>
2019-03-02 11:33:39 +0800

    complete example10, getchar implement pause.

---
+++ chapter4_examples/example10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Step 1:\n");
+  printf("Step 2:\n");
+  printf("Step 3:\n");
+  printf("按Enter键继续......");
+  getchar();
+  printf("Step 4:\n");
+  printf("Step 5:\n");
+  printf("Step 6:\n");
+  return 0;
+}
