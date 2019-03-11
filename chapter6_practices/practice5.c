7aa8ca51fa329a088fb7198a58745d57a97c9a48
carons-cr <2738794789@qq.com>
2019-03-11 16:06:36 +0800

    print 9x9 multiplication table.

---
+++ chapter6_practices/practice5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  for (int i = 1; i <= 9; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      printf("%d  ", i*j);
+    }
+    printf("\n");
+  }
+  return 0;
+}
