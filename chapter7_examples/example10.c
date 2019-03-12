17fbea9d69e28e06f20ea84328474d456088ea35
carons-cr <2738794789@qq.com>
2019-03-12 15:17:27 +0800

    complete example10, print characters in an  array.

---
+++ chapter7_examples/example10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  char c[] = {'I', ' ', 'a', 'm', ' ', 'a', ' ', 'g', 'i', 'r', 'l'};
+  for (int i = 0; i < 11; i++)
+    printf("%c", c[i]);
+  printf("\n");
+  return 0;
+}
