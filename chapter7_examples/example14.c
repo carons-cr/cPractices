a92ea7d30cc81dcf82f71d4539e9e0d166970a6e
carons-cr <2738794789@qq.com>
2019-03-14 14:06:44 +0800

    recomplete example14, absorb '\n' after each line break.

--- chapter7_examples/example14.c
+++ chapter7_examples/example14.c
@@ -5,23 +5,20 @@ int main()
 {
   char str1[80], min[80];
   int len;
-  printf("Input str1:\n");
-  scanf("%[^\n]", str1);
+  printf("输入若干字符串，以#为结束标志:\n");
+  scanf("%[^\n]%*c", str1);
   strcpy(min, str1);
   len = strlen(min);
 
-  printf("Input str1:\n");
-  getchar();
-  scanf("%[^\n]", str1);
-  while (str1[0] != '0')
+  scanf("%[^\n]%*c", str1);
+  while (str1[0] != '#')
   {
     if (strlen(str1) < len)
     {
       strcpy(min, str1);
       len = strlen(min);
     }
-    getchar();
-    scanf("%[^\n]", str1);
+    scanf("%[^\n]%*c", str1);
   }
   printf("len = %d, min = %s\n", len, min);
   return 0;
