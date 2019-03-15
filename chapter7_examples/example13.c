69797afa960add12f463bd32107cabf4b7b32642
carons-cr <2738794789@qq.com>
2019-03-15 17:42:12 +0800

    recomplete example13, catch two str.

--- chapter7_examples/example13.c
+++ chapter7_examples/example13.c
@@ -2,16 +2,14 @@
 
 int main()
 {
-  char str1[20], str2[20];
+  char str1[20];
+  char str2[20];
   int i;
   printf("请输入第一个字符串:\n");
   scanf("%[^\n]", str1);
   getchar();
   printf("请输入第二个字符串:\n");
   scanf("%[^\n]", str2);
-  
-  printf("str1 : %s\n", str1);
-  printf("str2 : %s\n", str2);
   
   for (i = 0; str1[i] != '\0'; i++);
   for (int j = 0; str2[j] != '\0'; j++)
