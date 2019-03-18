7ec7042e7051c4e905db6ddabbdfb69aa2076e7b
carons-cr <2738794789@qq.com>
2019-03-18 16:36:57 +0800

    complete example17, pointer point function as another function parameter.

---
+++ chapter9_examples/example17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+double cpe(double (*p1)(double), double (*p2)(double), double q)
+{
+  return (2*(*p1)(q) - (*p2)(2*q));
+}
+
+int main()
+{
+  double x;
+  double cpe();
+  x = 3.1415926/180;
+  printf("x = 15, y = %10.6f\n", cpe(sin, cos, 15*x));
+  printf("x = 30, y = %10.6f\n", cpe(sin, cos, 30*x));
+  printf("x = 45, y = %10.6f\n", cpe(sin, cos, 45*x));
+  return 0;
+}
